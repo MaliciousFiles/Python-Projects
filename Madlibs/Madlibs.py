696d79a949391a8607cfc1c13e172b68a50e45af
Maxim Rebguns <mrmaxguns@gmail.com>
2020-04-17 21:23:29 -0500

    Added comments even more

--- Madlibs/Madlibs.py
+++ Madlibs/Madlibs.py
@@ -5,31 +5,45 @@ Contributors: @mrmaxguns
 Copyright (c) 2020 MaliciousFiles All Rights Reserved.
 """
 
+# Imports
 from os import path
 import random
 
+
+# Get the template text file
 def get_template():
+    # Get the template file path
     file = input('Please enter the file you wish to use as a template (type "s" for the sample template): ')
+
+    # Automatic sample
     if file.lower() == 's':
         file = 'my_madlibs/sample.txt'
 
+    # Check if the file path is valid
     if path.exists(file):
         return file
     else:
         raise FileNotFoundError('the file "%s" was not found' % file)
 
+
+# Get all the words to be replaced from the template
 def get_words_from_template(file_path):
+    # Open and read the file
     file = open(file_path, 'r')
     file_lines = file.readlines()
     file_lines = ''.join(file_lines)
     file.close()
 
+    # Log the amount of nouns, verbs, and adjectives
     nouns = 0
     verbs = 0
     adjectives = 0
+
+    # Store other replaced word types in this list
     other = []
 
 
+    # Loop through the file to find all the 'blanks'/things being replaced
     for i in file_lines.split():
         if i == '[noun]':
             nouns += 1
@@ -42,19 +56,25 @@ def get_words_from_template(file_path):
         else:
             pass
 
+    # Return a list of all the 'blanks'/things being replaced
     return [nouns, verbs, adjectives, other]
 
+
+# Get the words from the user
 def grab_info(words_list):
+    # Get the 'blanks'
     nouns = words_list[0]
     verbs = words_list[1]
     adjectives = words_list[2]
     other = words_list[3]
 
+    # Create lists where the user-chosen words will be stored
     nounslist = []
     verbslist = []
     adjectiveslist = []
     other_words_dict = {}
 
+    # Ask the user for words to replace the 'blanks' and store them in the previously created variables
     if nouns != 0:
         nounslist = []
         for i in range(nouns):
@@ -75,20 +95,28 @@ def grab_info(words_list):
         for i in range(len(other)):
             other_words_dict[other[i]] = input('Give me a(n) %s: ' % other[i].replace('-', ' '))
 
+    # Return the lists in a list
     return [nounslist, verbslist, adjectiveslist, other_words_dict]
 
+
+# Generate the madlib
 def return_madlib(final_words_list, file):
+    # Get all the word lists
     nouns = final_words_list[0]
     verbs = final_words_list[1]
     adjectives = final_words_list[2]
     other = final_words_list[3]
 
+    # Read the file
     file = open(file, 'r')
     lines = file.readlines()
     lines = ''.join(lines)
     file.close()
 
+    # Create the list with all the words in the madlib
     final_list = []
+
+    # Copy the madlib to the final_list and replace the things in [] with the user-chosen words
     for i in lines.split():
         if i[0] == '[' and i[-1] == ']':
             if i == '[noun]':
@@ -111,14 +139,18 @@ def return_madlib(final_words_list, file):
         else:
             final_list.append(i)
 
+    # Return the string madlib by joining the final_list
     return ' '.join(final_list)
+    
 
+# Connect all of the functions together
 def do_the_mad_lib():
     template = get_template()
     info = grab_info(get_words_from_template(template))
     print(return_madlib(info, template))
 
 
+# Main loop
 print('Welcome to madlib!')
 while True:
     do_the_mad_lib()
