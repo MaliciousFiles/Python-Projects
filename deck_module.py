b6a0b97a98ef94fd0a4d97c9516781b29649f065
Maxim Rebguns <mrmaxguns@gmail.com>
2020-04-19 20:42:40 -0500

    Added lots of comments to deck_module.py

--- deck_module.py
+++ deck_module.py
@@ -1,6 +1,7 @@
 from random import choice, randint
 
 
+# Custom error
 class DeckError(Exception):
     def __init__(self, *args):
         if args:
@@ -16,52 +17,71 @@ class DeckError(Exception):
             return 'MyCustomError has been raised'
 
 
+# The card class
 class card:
+    # When the class is created:
     def __init__(self, suit, number):
-        if suit not in ['Spade','Club','Heart','Diamond', 'Joker']:
+        # Validate that the suit is an actual suit
+        if suit.lower() not in ['spade','club','heart','diamond', 'joker']:
             raise DeckError('Invalid suit: %s' % suit)
 
+        # Make sure number is an integer
         if number is not int:
             raise DeckError('number must be an integer')
 
+        # Make sure number is in range
         if not 0 < number < 14:
             raise DeckError('Invalid number: %s' % number)
 
+        # The suit isn't joker by default
         j = True
 
+        # Make long suit name capitalized
         long_suit = suit.capitalize()
 
+        # If the suit is a joker...
         if suit == 'Joker':
             j = False
             self.name = 'J'
             self.long_name = 'Joker'
 
+        # Add an s to the end of all long suits with no s
         if long_suit[-1]!='s':
             long_suit+='s'
 
+        # set suit to the first letter of suit lowercase
         suit = suit[0].lower()
+
+        # Set the value to number and long number to number
         self.value = number
         long_number = number
 
+        # Assign Ace, Jack, etc. to cards 1, 11, etc.
         if number == 1:
             number='A'
-        if number == 11:
+        elif number == 11:
             number='J'
-        if number == 12:
+        elif number == 12:
             number='Q'
-        if number == 13:
+        elif number == 13:
             number='K'
+
+        # Do the same as above but with long number
         if long_number == 1:
             long_number='Ace'
-        if long_number == 11:
+        elif long_number == 11:
             long_number='Jack'
         elif long_number == 12:
             long_number='Queen'
         elif long_number == 13:
             long_number='King'
+
+        # Set name and long name if the card is not a joker
         if j:
             self.long_name = str(long_number) + ' of ' + long_suit
             self.name = str(number) + suit
+
+        # Set the number and suit
         self.number = number
         self.suit = suit
 
@@ -69,29 +89,45 @@ class card:
         return self.name
 
 
+# Deck class
 class deck:
     def __init__(self, jokers=False):
+        # Set cards as empty list
         self.cards=[]
 
-        x=0
+        x = 0
+
+        # Do for every suit
         for suit in ['spade','club','heart','diamond']:
+            # Do for every number
             for number in range(1, 14):
+                # Add a card to the deck
                 self.cards += str(number)[0]
                 j = card(suit, number)
-                self.cards[(number-1)+(13*x)]=j
+                self.cards[(number - 1) + (13 * x)] = j
             x+=1
 
+        # Add a joker to deck if jokers is true
         if jokers:
             self.cards += '1'
             self.cards[-1] = card('Joker',0)
             self.cards+='1'
             self.cards[-1] = card('Joker',0)
 
+    # Draw a certain number of cards
     def draw(self, numofcards):
+        # Make sure numofcards is an integer
+        if numofcards is not int:
+            raise DeckError('numofcards must be an integer')
+
+        # Make sure the number of cards is a valid amount
         if numofcards > len(self.cards):
             raise DeckError('You asked for %s cards, but the deck only contains %s' % (numofcards, len(self.cards)))
 
+        # Set drawn_cards to a list
         self.drawn_cards=[]
+
+        # Cycle through until the number of cards is reached
         for x in range(0, numofcards):
             self.drawn_cards += '1'
 
@@ -104,6 +140,3 @@ class deck:
 
     def __repr__(self):
         return str(self.cards)
-
-c = card('Spade', 'oof')
-print(c)
